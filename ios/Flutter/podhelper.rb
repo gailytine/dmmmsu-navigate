d0aff1f8a4c9241bd44aad35e7b5b903ba3cbd2f
gailytine <galileejustinetigas@gmail.com>
2025-04-07 09:21:05 +0800

    Configure for iOS builds without local CocoaPods

---
+++ ios/Flutter/podhelper.rb
@@ -0,0 +1,7 @@
+﻿# This file is used by Flutter's iOS build system
+require 'fileutils'
+
+def install_all_flutter_pods(flutter_application_path)
+  flutter_root = File.join(flutter_application_path, '..', '..')
+  pod 'Flutter', :path => File.join(flutter_root, 'bin', 'cache', 'artifacts', 'engine', 'ios')
+end
